0b719cc9e9d9147d4f96692b19d93b6460914841
taehajs <taehajs@naver.com>
2026-01-06 16:59:26 +0900

    55

--- server.js
+++ server.js
@@ -31,3 +31,8 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+//Index Route
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" });
+});
